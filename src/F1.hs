57d0b0cedb4ce5e0b4f576d21ede9698ca3495e0
Jonatan Bölenius <boleni@kth.se>
2021-10-14 13:04:59 +0200

    Formatted code, finished function skyffla

--- src/F1.hs
+++ src/F1.hs
@@ -8,7 +8,7 @@ import Data.Char (isAlpha)
 -- 3: infib 1 2 3 = b = 2
 -- https://stackoverflow.com/questions/44874253/fibonacci-in-haskell
 -- rewritten so that it is easier to understand
-fib :: Int -> Int
+fib  :: Int -> Int
 fib 0 = 0
 fib 1 = 1
 fib n = 
@@ -19,16 +19,18 @@ fib n =
 
 -- Defining the consonants that are used 
 -- in rovarsprak & karpsravor
-consonants = "bcdfghjklmnpqrstvwxz"
-isConsonant :: Char -> Bool
-isConsonant x = x `elem` consonants
+isConsonant    :: Char -> Bool
+isConsonant x   = x `elem` consonants where
+    consonants  = "bcdfghjklmnpqrstvwxz"
 
-rovarsprak :: String -> String 
+rovarsprak   :: String -> String 
 rovarsprak [] = []
-rovarsprak [x] = if isConsonant x
-    then [x, 'o', x]
-    else [x]
-rovarsprak (x:xs) = rovarsprak [x] ++ rovarsprak xs
+rovarsprak [x] 
+    | isConsonant x = [x, 'o', x]
+    | otherwise     = [x]
+rovarsprak (x:xs) = rovarsprak [x] ++ rovarsprak xs  -- I was worried that take unneccesary computing
+-- but since the tail xs is being added to rovarsprak [x] = [x, 'o', x] if x is cons then it should max require 3 times the amount of letters
+-- of the given word in operations
 
 -- Why this makes sense
 -- Ex       "lololol"
@@ -42,26 +44,88 @@ rovarsprak (x:xs) = rovarsprak [x] ++ rovarsprak xs
 -- returns 'l' : karpsravor ""
 -- karpsravor "" = "" per base case definition
 -- Final return is therefore 'lol', which is correct
-karpsravor :: String -> String
-karpsravor [] = []
-karpsravor (x:xs) = if isConsonant x
+karpsravor         :: String -> String
+karpsravor []       = []
+karpsravor (x:xs)   = if isConsonant x
     then x : karpsravor (drop 2 xs)
     else x : karpsravor xs
 
-split :: String ->  [String]
-split [] = []
-split s = clean (inSplit [] "" s) where
-    inSplit :: [String] -> String -> String -> [String]
-    inSplit a cS ""     = cS:a
+split      :: String ->  [String]
+split []    = []
+split s     = clean (inSplit [] "" s) where
+    inSplit        :: [String] -> String -> String -> [String]
+    inSplit a cS "" = cS:a
     inSplit a cS (x:xs)
         | isAlpha x     = inSplit a (cS ++ [x]) xs
         | otherwise     = inSplit (cS:a) "" xs
-    clean [] = []
-    clean a = [x | x <- a, not (null x)]
+    clean []        = []
+    clean a         = [x | x <- a, not (null x)]
 
-medellangd :: String -> Double
-medellangd [] = 0
-medellangd s = fromIntegral(sum [sum [1 | _ <- xs] | xs <- split s]) / fromIntegral(length (split s))
+medellangd     :: String -> Double
+medellangd []   = 0
+medellangd s    = fromIntegral(sum [sum [1 | _ <- xs] | xs <- split s]) / fromIntegral(length (split s))
 
 
--- skyffla osv
+{- too slow but works
+skyffla     :: [a] -> [a]
+skyffla []   = []
+skyffla all  = let
+    inSkyffla          :: Int -> [a] -> [a] -> [a] -> [a]
+    inSkyffla i a [] [] = a
+    inSkyffla i a [] rA = inSkyffla 0 a rA []
+    inSkyffla i a (x:xs) rA
+        | even i    = inSkyffla (i-1) (a++[x]) xs rA
+        | otherwise = inSkyffla (i-1) a xs (rA++[x])
+    in inSkyffla (length all - 1) [] all [] -}
+
+
+
+skyffla     :: [a] -> [a]
+skyffla []   = []
+skyffla all  = let
+    inSkyffla              :: [a] -> [a] -> [a] -> [a]
+    inSkyffla a [] []               = reverse a
+    inSkyffla a [] rA               = inSkyffla a (reverse rA) []
+    inSkyffla a (f:s:xs) rA         = inSkyffla (f:a) xs (s:rA)
+    inSkyffla a (f:xs) rA           = inSkyffla (f:a) (reverse rA) []
+    in inSkyffla [] all []
+
+
+{- All code below this point is just to make it clear to myself  what is happening
+    inSkyffla [] [1,2,3,4] []   -->
+    inSkyffla [1] [3,4] [2]     -->
+    inSkyffla [3,1] [] [4,2]    -->
+    inSkyffla [3,1] [4,2] []    -->
+    inSkyffla [2,3,1] [] [4]    -->
+    inSkyffla [2,3,1] [4] []    -->
+    inSkyffla [4,2,3,1] [] []   -->
+
+    
+    inSkyffla [] [1,2,3,4,5] [] 
+    inSkyffla [1] [3,4,5] [2]       
+    inSkyffla [1,3] [5] [2,4]
+    inSkyffla [1,3,5] [5] [2,4]
+-}
+
+{- doesn't work
+skyffla     :: [a] -> [a]
+skyffla []   = []
+skyffla all  = let
+    inSkyffla              :: Int -> Int -> [a] -> [a]
+    inSkyffla i m all@(x:xs)
+        | m == -1    = all
+        | i == m+1    = inSkyffla 0 m all
+        | even i    = inSkyffla (i+1) (m-1) (xs++[x])
+        | otherwise = inSkyffla (i+1) (m-1) (xs++[all!!i])
+    in inSkyffla 0 (length all-1) all -}
+
+    -- inSkyffla 0 3 [1,2,3,4] --> inSkyffla 1 2 [2,3,4,1]
+    -- inSkyffla 1 2 [2,3,4,1] --> inSkyffla 2 1 [2,4,1,3]
+    -- inSkyffla 2 1 [2,3,4,1] --> inSkyffla 0 1 [2,4,1,3]
+    -- inSkyffla 0 1 [2,3,1,3] --> inSkyffla 1 0 [4,1,3,2]
+    -- inSkyffla 1 0 [4,1,3,2] --> inSkyffla 0 0 [4,1,3,2]
+    -- inSkyffla 0 0 [4,1,3,2] --> inSkyffla 1 -1 [1,3,2,4]
+
+    -- inSkyffla 0 3 [1,2,3,4] --> inSkyffla 1 2 [2,3,4,1]
+    -- inSkyffla 1 2 [2,3,4,1] --> inSkyffla 2 2 [2,3,4,1]
+    -- inSkyffla 2 2 [2,3,4,1] --> inSkyffla 3 2 [2,4,1,3]
